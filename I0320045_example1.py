38ecd9871eef36899de0e8bcf16faebb081ab814
HarryBrahmana <80480107+HarryBrahmana@users.noreply.github.com>
2021-04-29 16:21:15 +0700

    Add files via upload
    Harry-Brahmana_I0320045_Andhika-Pratama-Putra_Modul-9

---
+++ I0320045_example1.py
@@ -0,0 +1,12 @@
+#importing "array"
+import array
+
+>>> #inisialisasi variabel berformat array
+>>> Angka = [1, 2, 3, 4]
+>>> Nama = ["Dono", "Kasino", "Indro"]
+>>> print Nama
+>>> print Angka
+
+Output:
+['Dono', 'Kasino', 'Indro']
+[1, 2, 3, 4]
